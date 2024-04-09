c272c85ebe83d55a5716ae7ac1f3c426bd845396
Andrej Spinej <enjoyhillol@gmail.com>
2024-04-09 20:27:30 +1000

    add testimonialst block + contact block

--- assets/src/js/testimonials.js
+++ assets/src/js/testimonials.js
@@ -1,20 +1,27 @@
 fetch('../../../assets/testimonials/testimonials.json')
     .then((response) => response.json())
     .then((data) => {
-        let testimonialsContainer = document.querySelector(
-            '.testimonials-wrapper'
-        )
+        let swiperWrapper = document.querySelector('.swiper-wrapper')
+        swiperWrapper.innerHTML = '' // Очистите текущие слайды
+
         data.forEach((testimonial) => {
-            let testimonialCard = `
-        <div class="testimonials-card">
-          <div class="testimonials-card__title">
-            <img class="testimonials-img" src="${testimonial.image}" alt="фото клиента" />
-            <h6>${testimonial.name}</h6>
-          </div>
-          <span>${testimonial.date}</span>
-          <p>${testimonial.testimonial}</p>
-        </div>
-      `
-            testimonialsContainer.innerHTML += testimonialCard
+            let slide = `
+            <div class="swiper-slide">
+                <div class="testimonials-card">
+                    <div class="testimonials-card__title">
+                        <img class="testimonials-img" src="${testimonial.image}" alt="фото клиента" />
+                        <h6>${testimonial.name}</h6>
+                    </div>
+                    <span>${testimonial.date}</span>
+                    <p>${testimonial.testimonial}</p>
+                </div>
+            </div>
+            `
+
+            swiperWrapper.innerHTML += slide
         })
+
+        // После того как все слайды добавлены, обновите Swiper
+        var swiper = new Swiper('.swiper-container')
+        swiper.update()
     })
